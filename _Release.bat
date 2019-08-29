21912ecd3350c38eab79793c3d8e6d2942368cfe
stackprobe <stackprobes@gmail.com>
2019-08-29 09:13:02 +0900

    Dream

--- _Release.bat
+++ _Release.bat
@@ -12,7 +12,7 @@ COPY /B VioletDiamond\VioletDiamond\bin\Release\DxLibDotNet.dll out
 C:\Factory\Tools\xcp.exe doc out
 C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
 
-C:\Factory\SubTools\zip.exe /G out VioletDiamond
+C:\Factory\SubTools\zip.exe /PE- /G out VioletDiamond
 C:\Factory\Tools\summd5.exe /M out
 
 PAUSE
