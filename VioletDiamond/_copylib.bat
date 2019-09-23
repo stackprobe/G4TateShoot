eb4a7bdfafb5b6a69d0c9987bf69618e499d3d6d
stackprobe <stackprobes@gmail.com>
2019-09-24 02:45:19 +0900

    Pine

--- VioletDiamond/_copylib.bat
+++ VioletDiamond/_copylib.bat
@@ -1,2 +1,3 @@
 C:\Factory\SubTools\CopyLib.exe C:\Dev\Fairy\Donut2\Donut2\Donut2\Common VioletDiamond\Common
+C:\Factory\SubTools\CsProjAdjustCs.exe VioletDiamond Common
 ROBOCOPY C:\Dev\Fairy\Donut2\Resource\General ..\Resource\General /MIR
