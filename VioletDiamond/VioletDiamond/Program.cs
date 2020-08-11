de03675d8b1d805396e5d8741b49ae142c1561b7
stackprobe <stackprobes@gmail.com>
2020-08-12 07:44:37 +0900

    Melody

--- VioletDiamond/VioletDiamond/Program.cs
+++ VioletDiamond/VioletDiamond/Program.cs
@@ -17,7 +17,7 @@ namespace Charlotte
 			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
 		}
 
-		public const string APP_IDENT = "{7a686399-df89-44cc-9a43-2e20974bbb55}";
+		public const string APP_IDENT = "{dab81ae0-6731-4c26-b8e8-27ffd23fd0c8}";
 		public const string APP_TITLE = "VioletDiamond";
 	}
 }
